7e1a6222072d48fe45c6ec9e73f59066d98cacd3
shariram <mhsankarlal@gmail.com>
2017-01-29 00:34:57 +0800

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 gem 'bootstrap-sass', '3.3.6'
+gem 'bcrypt',         '3.1.11'
 # Use pg as the database for Active Record
 gem 'pg'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115035945) do
+ActiveRecord::Schema.define(version: 20170128155950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170115035945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
   add_foreign_key "comments", "articles"
